97991574204bc5a030de7762af1ea64978ed8254
Jacob Cook <jacob@peakwinter.net>
2017-11-30 20:42:14 -0500

    Push to Docker Hub

---
+++ deploy/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+docker pull jekyll/jekyll
+docker run --rm --volume "$PWD:/srv/jekyll" -e "JEKYLL_UID=$UID" -it jekyll/jekyll jekyll build
+
+if [ ! "env:$TRAVIS_BRANCH" == "env:master" ]; then
+    echo "not on master, not pushing"
+    exit 0
+fi
+
+docker build -t $DOCKER_REPO:latest .
+
+if [ "env:$TRAVIS_BRANCH" == "env:master" ]; then
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+  docker push $DOCKER_REPO:latest
+fi
